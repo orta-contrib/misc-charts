f5adaf1468c4391139341309b1647ace51ba7b7a
orta-contrib <orta.contrib@gmail.com>
2020-12-29 12:17:02 +0900

    render icons

--- dataSheet.js
+++ dataSheet.js
@@ -4,30 +4,80 @@ class DataSheet {
         if (!sheetName) {
             sheetName = workbook.SheetNames[0];
         }
-        this.worksheet = workbook.Sheets[sheetName];
+        const worksheet = workbook.Sheets[sheetName];
+        this.data = XLSX.utils.sheet_to_json(worksheet, {header: "A"});
     }
 
-    readAsChartModel(range) {
-        const data = XLSX.utils.sheet_to_json(this.worksheet, {range: range, header: "A"});
-        return data.map(d => new ChartModel(d));
+    readAsDataset(rowNames, startCol) {
+        const dataToRender = [];
+        for (var i = 0; i < this.data.length; i++) {
+            if (typeof rowNames == 'string') {
+                dataToRender.push(this.data[i][rowNames]);
+            } else if (rowNames instanceof Array) {
+                const tmp = [];
+                for (var j = 0; j < rowNames.length; j++) {
+                    tmp.push(this.data[i][rowNames[j]]);
+                }
+                dataToRender.push(tmp);
+            } else {
+                dataToRender.push({});
+            }
+        }
+        dataToRender.splice(0, startCol);
+        return dataToRender;
+    }
+
+    loadIconImage(url) {
+        // TODO: Make default icon
+        const defaultIcon = new Image(48,48);
+        return new Promise((resolve, reject) => {
+            const icon = new Image(48,48);
+            icon.onload = () => resolve(icon);
+            icon.onerror = (err) => resolve(defaultIcon);
+            icon.src = url;
+        })
+    }
+    async readAsIconDataset(rowName, startCol) {
+        return this.data.slice(startCol).map(async col => {
+            return await this.loadIconImage(`/data/icons/${col[rowName]}.png`);
+        });
     }
 }
 
 class ChartModel {
-    constructor(sheetJson) {
-        this.data = sheetJson;
+    constructor() {
+        // TODO: parameterize
+        this.iconRow = "A";
+        this.labelRow = "C";
+        this.renderRows = [
+            {type: "bar", row: ["BS", "BU"]},
+            {type: "line", row: "BT"}
+        ];
+        this.startCol = 4;
     }
 
-    toDataset(rowNames) {
-        if (typeof rowNames == 'string') {
-            return this.data[rowNames];
-        } else if (rowNames instanceof Array) {
-            const tmp = [];
-            for (var j = 0; j < rowNames.length; j++) {
-                tmp.push(this.data[rowNames[j]]);
-            }
-            return tmp
-        }
-        return -1;
+    async render(chart, sheet) {
+        // Initialize
+        chart.clear();
+
+        const labels = sheet.readAsDataset(this.labelRow, this.startCol);
+        const datasets = this.renderRows.map(rrow => {
+            const dataToRender = sheet.readAsDataset(rrow.row, this.startCol);
+            return {
+                type: rrow.type,
+                data: dataToRender
+            };
+        });
+        chart.render(labels, datasets);
+
+        // Icons will renader after rendering charts, because loading icons would take time 
+        const icons = await sheet.readAsIconDataset(this.iconRow, this.startCol);
+        Promise.all(icons).then(values => {
+            chart.append({
+                type: "line",
+                data: Array.from({length: icons.length}, () => 0),
+                pointStyle: values
+            });
+        });
     }
 }
--- deposeChart.js
+++ deposeChart.js
@@ -10,11 +10,11 @@ class DeposeChart {
             labels: Array.from({length: 100}, () => 'man'),
             datasets: []
         }
-       //  this.chart.render();
     };
 
-    render(dataset) {
-        this.chart.data.datasets.push(dataset);
+    render(labels, dataset) {
+        this.chart.data.labels = labels;
+        this.chart.data.datasets = this.chart.data.datasets.concat(dataset);
         this.chart.options = {
             tooltips: {
                 callbacks: {
@@ -32,5 +32,15 @@ class DeposeChart {
         this.chart.canvas.parentNode.style.width = (48 * this.chart.data.labels.length) + 'px';
         this.chart.update();
         this.chart.resize();
+    }
+
+    append(dataset) {
+        this.chart.data.datasets.push(dataset);
+        this.chart.update();
+        this.chart.resize();
+    }
+
+    clear() {
+        this.chart.clear();
     }
 }
