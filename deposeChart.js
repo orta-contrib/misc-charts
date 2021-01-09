22d56d76c2d178e89d2fd0ad897ced16b7cf8923
orta-contrib <orta.contrib@gmail.com>
2021-01-09 21:44:48 +0900

    fix: disable tooltip

--- deposeChart.js
+++ deposeChart.js
@@ -8,6 +8,7 @@ class DeposeChart {
         this.chart.data.labels = labels.map(l => ['  '].concat(l.split(''))); // Hack for adjusting ticks (x-axis label)
         this.chart.data.datasets = datasets;
         this.chart.options = {
+            tooltips: {enabled: false},
             maintainAspectRatio: false
         };
         this.chart.canvas.parentNode.style.width = (48 * this.chart.data.labels.length) + 'px';
